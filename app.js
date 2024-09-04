c2e11ca3526afa139a2e7ca6d2694d3dfb0911b1
Algoruu <zlfkcmspdbdp@naver.com>
2024-09-04 19:24:23 +0900

    유튜버 데모 업데이트

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use(express.json())//미들웨어(http 외 모듈) 사용
+// express.json -> express 모듈 중 하나. json이라는 미들웨어를 사용하면
+// request로 날아오는 body값을 json으로 읽어볼 수 있음!
+app.post('/test', (req, res) => {
+  //body에 숨겨져서 들어온 데이터를 화면에 나타내줄 수 있음!
+  console.log(req.body.message)
+
+  res.json(req.body)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- demo-api/youtuber-demo.js
+++ demo-api/youtuber-demo.js
@@ -28,11 +28,20 @@ let youtuber3 = {
 }
 
 let db = new Map()
-db.set(1, youtuber1) 
-db.set(2, youtuber2)
-db.set(3, youtuber3)
+var id = 1
+
+db.set(id++, youtuber1) //key - value쌍 = json과 비슷!
+db.set(id++, youtuber2)
+db.set(id++, youtuber3)
 
 // REST API 설계
+app.get('/youtubers', function (req, res) {
+    //req:X
+    res.json({
+        message : "test"
+    })
+})
+
 app.get('/youtubers/:id', function (req, res) {
     let {id} = req.params
     //params는 항상 문자열! 따라서 parseInt로 감싸주기
@@ -46,3 +55,18 @@ app.get('/youtubers/:id', function (req, res) {
         res.json(youtuber)
     }
 })
+
+app.use(express.json())//미들웨어(http 외 모듈) json 설정
+// express.json -> express 모듈 중 하나. json이라는 미들웨어를 사용하면
+// request로 날아오는 body값을 json으로 읽어볼 수 있음!
+app.post('/youtubers', (req, res) => {
+    // 등록?  Map(db)에 저장(PUT) 해줘야함
+    db.set(id++, req.body)
+
+    res.json({
+        message : `${db.get(id-1).channelTitle}님, 유튜브 채널 생성을 축하합니다!`
+        // request.body에서 그대로 channelTitle 값을 가져오거나
+        // db에서 저장이 잘 됐나 확인 겸 값 꺼내기
+        //여기선 db에서 값 가져옴!
+    })
+}) 
